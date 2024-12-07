b64f4e932de89357020c2e6640b43a89fbb2b966
patient4 <rachit.mohan2112@gmail.com>
2024-12-08 00:37:00 +0530

    added client logic

--- client.py
+++ client.py
@@ -9,7 +9,8 @@ from Model.model_manager import dataclass_to_grpc_obj
 
 channel = grpc.insecure_channel('localhost:5001')
 stub = inventory_service_pb2_grpc.InventoryServiceStub(channel)
-product_data = [{
+product_data = [
+    {
     "id": 1,
     "name": "Fair and lovely",
     "description": "Good cream",
@@ -22,26 +23,58 @@ product_data = [{
      "stock": 10,
      "price": 100
      },
-{"id":2,
+    {"id":2,
      "name":"Fena",
      "description":"mard ko banae kala",
      "stock": 10,
      "price": 100
-     }
+     },
+    {
+        "id": 3,
+        "name":"Vasmol",
+        "description":"Surakshit kale mere baal vasmol ne kiya kamaal",
+        "stock": 10,
+        "price": 110
+    }
 ]
 
-# product = Product(**product_data[2])
-# print(product)
-# productRequest = dataclass_to_grpc_obj(product)
-# print(productRequest)
-# response = stub.UpdateProduct(productRequest)
-# response = stub.AddProduct(dataclass_to_grpc_obj(product))
+product = Product(**product_data[3])
+print(product)
+#add product logic
+
+# request = dataclass_to_grpc_obj(product)
+# response = stub.AddProduct(request)
+# print(response)
+
+## delete product logic
 # id = 1
 # request = inventory_service_pb2.ProductRequest(id=id)
 # response = stub.DeleteProduct(request)
+# print(response)
+
+# update product logic
+# id = 1
+# product_update = {
+#     "id": "2",
+#     "name": "Tata Tea",
+#     "description": "waah TAJ!",
+#     "stock": 10,
+#     "price": 100
+# }
+#
+# product = Product(**product_update)
+# request = dataclass_to_grpc_obj(product)
+# response = stub.UpdateProduct(request)
+# print(response)
+
+# # get product with id=3
+# id = 3
+# request = inventory_service_pb2.ProductRequest(id=id)
+# response = stub.GetProduct(request)
+# print(response)
+
+# # get all product from the entity products
 # request = inventory_service_pb2.Empty()
 # response = stub.GetAllProducts(request)
-id = 2
-request = inventory_service_pb2.ProductRequest(id=id)
-response = stub.GetProduct(request)
-print(response)
+# print(response)
+
